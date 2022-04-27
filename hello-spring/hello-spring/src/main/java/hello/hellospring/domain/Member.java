75fb4aed7dce971a19c81c541ff2ee1897ca06a2
minyeong <ninja2319@naver.com>
2022-04-27 22:25:38 +0900

    Section5,6

--- hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,16 @@
 package hello.hellospring.domain;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.springframework.stereotype.Controller;
+
+import javax.persistence.*;
+
+@Entity
+
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //데이터를 구분하기 위해 시스템이 저장하는 아이디
+
     private String name;
 
     public Long getId() {
